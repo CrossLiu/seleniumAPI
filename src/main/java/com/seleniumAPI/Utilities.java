f9888600f4a6cb3ad7f46bcb42b897818d0c4c68
CrossLiu <cross_0902@163.com>
2018-12-13 17:28:49 +0800

    update

--- src/main/java/com/seleniumAPI/Utilities.java
+++ src/main/java/com/seleniumAPI/Utilities.java
@@ -1,5 +1,7 @@
 package com.seleniumAPI;
 
+
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -10,7 +12,7 @@ import org.openqa.selenium.remote.BrowserType;
 import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-import com.gargoylesoftware.htmlunit.javascript.host.Set;
+
 
 
 /**
@@ -20,24 +22,17 @@ import com.gargoylesoftware.htmlunit.javascript.host.Set;
  */
 public class Utilities {
 	
-	 private static WebDriver driver;
-		/**
-		 * 
-		 * @param browserName:internet explorer,firefox,chrome
-		 * @return
-		 */
-	    public static WebDriver getDriver(String browserName) {
-			if (browserName.equalsIgnoreCase(BrowserType.CHROME)) {
-				driver=new ChromeDriver();
-			} else if (browserName.equalsIgnoreCase(BrowserType.IE)) {
-				driver=new InternetExplorerDriver();
-			}else if (browserName.equalsIgnoreCase(BrowserType.FIREFOX)) {
-				driver=new FirefoxDriver();
-			}
-			return driver;
-		}
+	private static  WebDriver driver; 
+	public Utilities(WebDriver driver) {
+		Utilities.driver=driver;
+	}
+		
+
+	    
+  
+
 	//判断元素是否存在
-	public static boolean isElementPresent(WebDriver driver,final By by,long timeout) {
+	public boolean isElementPresent(final By by,long timeout) {
 		boolean flag=true;
 		WebDriverWait wait=new WebDriverWait(driver, timeout);
 		try {
@@ -56,28 +51,8 @@ public class Utilities {
 		
 		return flag;
 	}
-	//判断元素是否存在, 使用公共变量driver=getDriver()
-	public static boolean isElementPresent(final By by,long timeout) {
-		boolean flag=true;
-		WebDriverWait wait=new WebDriverWait(driver, timeout);
-		try {
-			wait.until(new ExpectedCondition<WebElement>() {
-
-				public WebElement apply(WebDriver driver) {
-					// TODO Auto-generated method stub
-					return driver.findElement(by);
-				}
-			});
-			flag=true;
-		} catch (Exception e) {
-			// TODO: handle exception
-			flag=false;
-		}
-		
-		return flag;
-	}
-	//判断元素是否存在，另外一种方式
-		public static boolean isElementPresence(WebDriver driver,final By by,long timeout) {
+		//判断元素是否存在，另外一种方式
+		public  boolean isElementPresence(final By by,long timeout) {
 			boolean flag=true;
 			WebDriverWait wait=new WebDriverWait(driver, timeout);
 			try {
@@ -98,7 +73,7 @@ public class Utilities {
 		}
 		
 		//判断元素是否可见，如按钮，checkbox，radiobox input，select，和textarea上
-		public static boolean isElementEnabled(WebDriver driver,final By by,long timeout) {
+		public  boolean isElementEnabled(final By by,long timeout) {
 			boolean flag=true;
 			WebDriverWait wait=new WebDriverWait(driver, timeout);
 			try {
@@ -119,7 +94,7 @@ public class Utilities {
 		}
 		
 		//判断元素是否被选中，如按钮，checkbox，radiobox input，select，和textarea上
-				public static boolean isElementSelected(WebDriver driver,final By by,long timeout) {
+				public boolean isElementSelected(final By by,long timeout) {
 					boolean flag=true;
 					WebDriverWait wait=new WebDriverWait(driver, timeout);
 					try {
@@ -140,7 +115,7 @@ public class Utilities {
 				}
 				
 				//切换窗口通用方法
-				public void switchToWindow(WebDriver driver,String windowsTitle) {
+				public  void switchToWindow(String windowsTitle) {
 					java.util.Set<String> windowHanders=driver.getWindowHandles();
 					for (String hander : windowHanders) {
 						driver.switchTo().window(hander);
--- src/main/java/com/seleniumAPI/WaitAPI.java
+++ src/main/java/com/seleniumAPI/WaitAPI.java
@@ -1,29 +1,27 @@
 package com.seleniumAPI;
 
 import java.util.concurrent.TimeUnit;
-
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.ExpectedCondition;
-import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
-import org.testng.annotations.Test;
+
 
 public class WaitAPI {
-	@Test
-	public void waitAPI() {
-		WebDriver driver=new FirefoxDriver();
-		driver.get("http://www.baidu.com");
-		//隐式等待5秒，对本类中每个查找元素都起作用；先在查元素，如果没找到，在等待5秒再查找一遍
-		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
-		driver.findElement(By.id("kw")).sendKeys("WebDriver");
+//	@Test
+	//public void waitAPI() {
+//		WebDriver driver=new FirefoxDriver();
+//		driver.get("http://www.baidu.com");
+//		//隐式等待5秒，对本类中每个查找元素都起作用；先在查元素，如果没找到，在等待5秒再查找一遍
+//		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+//		driver.findElement(By.id("kw")).sendKeys("WebDriver");
 		
+
 		//显式等待
-		WebDriverWait wait=new WebDriverWait(driver, 10);//10秒没找到抛异常
-		WebElement element=wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector("input[id='su']")));
-		element.click();
+//		WebDriverWait wait=new WebDriverWait(driver, 10);//10秒没找到抛异常
+//		WebElement element=wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector("input[id='su']")));
+//		element.click();
 		
 //		wait.until(new ExpectedCondition<WebElement>() {
 //			public WebElement apply(WebDriver arg0) {
@@ -31,12 +29,17 @@ public class WaitAPI {
 //				return arg0.findElement(By.cssSelector("input[id='su']"));
 //			}
 //		});
-		boolean flag=isElementPresent(driver, By.cssSelector("input[id='su']"), 5);
-		System.out.println(flag);
-		}
+//		boolean flag=isElementPresent(driver, By.cssSelector("input[id='su']"), 5);
+//		System.out.println(flag);
+//		}
 	
+	private WebDriver driver;
+	
+	public WaitAPI(WebDriver driver) {
+		this.driver=driver;
+	}
 	//判断元素是否存在
-	public static boolean isElementPresent(WebDriver driver,final By by,long timeout) {
+	public boolean isElementPresent(final By by,long timeout) {
 		boolean flag=true;
 		WebDriverWait wait=new WebDriverWait(driver, timeout);
 		try {
@@ -55,6 +58,27 @@ public class WaitAPI {
 		
 		return flag;
 	}
+	
+	//判断元素是否可见，如按钮，checkbox，radiobox input，select，和textarea上
+			public boolean isElementEnabled(final By by,long timeout) {
+				boolean flag=true;
+				WebDriverWait wait=new WebDriverWait(driver, timeout);
+				try {
+					flag=wait.until(new ExpectedCondition<Boolean>() {
+
+						public Boolean apply(WebDriver driver) {
+							// TODO Auto-generated method stub
+							return driver.findElement(by).isEnabled();
+						}
+					});
+				
+				} catch (Exception e) {
+					// TODO: handle exception
+					flag=false;
+				}
+				
+				return flag;
+			}
 	}
 
 
